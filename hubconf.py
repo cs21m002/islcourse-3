8668e03cdaf75632e4be0cd136c431067f6ca6b4
cs21m009iittp <89476788+cs21m009iittp@users.noreply.github.com>
2022-10-19 10:58:04 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,2 +1,9 @@
-def sample_fun():
-  print('x')
+import torch
+from torch import nn
+
+def kali():
+  print ('kali')
+  
+# Define a neural network YOUR ROLL NUMBER (all small letters) should prefix the classname
+class cs21m009NN(nn.Module):
+  pass
