53516b7f1665b1557f18f87d84ce3367236bb764
cs21m009iittp <89476788+cs21m009iittp@users.noreply.github.com>
2022-10-19 11:46:03 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -37,7 +37,105 @@ def get_model(train_data_loader=None, n_epochs=10):
 
   criterion = nn.CrossEntropyLoss()
   optimizer = optim.SGD(cs21m009NN.parameters(), lr=0.001, momentum=0.9)
-  
+  model = cs21m009NN
+
+  for epoch in range(n_epochs):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(train_dat_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = cs21m009NN(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+  print('Finished Training')
   print ('Returning model... (rollnumber: cs21m009)')
   
   return model
+
+def get_model_advanced(train_data_loader=None, n_epochs=10,lr=1e-4,config=None):
+  model = None
+
+  # write your code here as per instructions
+  # ... your code ...
+  # ... your code ...
+  # ... and so on ...
+  # Use softmax and cross entropy loss functions
+  # set model variable to proper object, make use of train_data
+  
+  # In addition,
+  # Refer to config dict, where learning rate is given, 
+  # List of (in_channels, out_channels, kernel_size, stride=1, padding='same')  are specified
+  # Example, config = [(1,10,(3,3),1,'same'), (10,3,(5,5),1,'same'), (3,1,(7,7),1,'same')], it can have any number of elements
+  # You need to create 2d convoution layers as per specification above in each element
+  # You need to add a proper fully connected layer as the last layer
+  
+  # HINT: You can print sizes of tensors to get an idea of the size of the fc layer required
+  # HINT: Flatten function can also be used if required
+  return model
+  
+  
+  print ('Returning model... (rollnumber: xx)')
+  
+  return model
+
+
+# sample invocation torch.hub.load(myrepo,'test_model',model1=model,test_data_loader=test_data_loader,force_reload=True)
+def test_model(model1=None, test_data_loader=None):
+
+  accuracy_val, precision_val, recall_val, f1score_val = 0, 0, 0, 0
+  # write your code here as per instructions
+  # ... your code ...
+  # ... your code ...
+  # ... and so on ...
+  # calculate accuracy, precision, recall and f1score
+
+  with torch.no_grad():
+    for data in test_data_loader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = model1(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+  
+  print(f'Accuracy of the network : {100 * correct // total} %')
+
+  correct_pred = {classname: 0 for classname in labels}
+  total_pred = {classname: 0 for classname in labels}
+
+  # again no gradients needed
+  with torch.no_grad():
+      for data in test_data_loader:a
+          images, labels = data
+          outputs = model1(images)
+          _, predictions = torch.max(outputs, 1)
+          # collect the correct predictions for each class
+          for label, prediction in zip(labels, predictions):
+              if label == prediction:
+                  correct_pred[labels[label]] += 1
+              total_pred[labels[label]] += 1
+
+
+  # print accuracy for each class
+  for classname, correct_count in correct_pred.items():
+      accuracy = 100 * float(correct_count) / total_pred[classname]
+      print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
+
+  print ('Returning metrics... (rollnumber: cs21m0009)')
+  
+  return accuracy_val, precision_val, recall_val, f1score_val
