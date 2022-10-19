e505ec8547b4bec0431cf909a1e919edb81c3e37
cs21m009iittp <89476788+cs21m009iittp@users.noreply.github.com>
2022-10-19 11:31:06 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
 def kali():
   print ('kali')
@@ -9,22 +10,34 @@ def kali():
 class cs21m009NN(nn.Module):
     def __init__(self):
       super(cs21m009NN, self).__init__()
-      # 1 input image channel, 6 output channels, 5x5 square convolution
-      # kernel
       self.conv1 = nn.Conv2d(1, 6, 5)
       self.conv2 = nn.Conv2d(6, 16, 5)
-      # an affine operation: y = Wx + b
-      self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5*5 from image dimension
+      self.fc1 = nn.Linear(16 * 5 * 5, 120)
       self.fc2 = nn.Linear(120, 84)
       self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-      # Max pooling over a (2, 2) window
       x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-      # If the size is a square, you can specify with a single number
       x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-      x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
+      x = torch.flatten(x, 1)
       x = F.relu(self.fc1(x))
       x = F.relu(self.fc2(x))
       x = self.fc3(x)
       return x
+
+def get_model(train_data_loader=None, n_epochs=10):
+  model = None
+
+  # write your code here as per instructions
+  # ... your code ...
+  # ... your code ...
+  # ... and so on ...
+  # Use softmax and cross entropy loss functions
+  # set model variable to proper object, make use of train_data
+
+  criterion = nn.CrossEntropyLoss()
+  optimizer = optim.SGD(cs21m009NN.parameters(), lr=0.001, momentum=0.9)
+  
+  print ('Returning model... (rollnumber: cs21m009)')
+  
+  return model
