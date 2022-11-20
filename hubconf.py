3f9f8f93e052c1eeca23868f55e7bd049d2bf18b
Nikhil-Malleti <89476788+Nikhil-Malleti@users.noreply.github.com>
2022-11-21 01:30:08 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,11 @@
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, ToPILImage
+from PIL import Image
+import numpy as np
+
 class my_model(nn.Module):
     def __init__(self,size):
         super(my_model,self).__init__()
@@ -13,7 +21,30 @@ class my_model(nn.Module):
         x = self.net_stack(x)
         return x
       
-      
+
+
+
+def conv2d_output_size(input_size, out_channels, padding, kernel_size, stride, dilation=None):
+    """According to https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
+    """
+    if dilation is None:
+        dilation = (1, ) * 2
+    if isinstance(padding, int):
+        padding = (padding, ) * 2
+    if isinstance(kernel_size, int):
+        kernel_size = (kernel_size, ) * 2
+    if isinstance(stride, int):
+        stride = (stride, ) * 2
+
+    output_size = (
+        out_channels,
+        np.floor((input_size[1] + 2 * padding[0] - dilation[0] * (kernel_size[0] - 1) - 1) /
+                 stride[0] + 1).astype(int),
+        np.floor((input_size[2] + 2 * padding[1] - dilation[1] * (kernel_size[1] - 1) - 1) /
+                 stride[1] + 1).astype(int)
+    )
+    return output_size
+        
 def get_model(train_loader):
 
   c_i, c_o = 3, 20
