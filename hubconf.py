0775cd829d650fd771701ca8efa4ecbd924cbbe8
Nikhil-Malleti <89476788+Nikhil-Malleti@users.noreply.github.com>
2022-11-21 09:46:33 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,29 +1,14 @@
 import torch
 from torch import nn
 import torch.optim as optim
-
-# You can import whatever standard packages are required
-
-# full sklearn, full pytorch, pandas, matplotlib, numpy are all available
-# Ideally you do not need to pip install any other packages!
-# Avoid pip install requirement on the evaluation program side, if you use above packages and sub-packages of them, then that is fine!
-
-###### PART 1 ######
+from sklearn.datasets import make_blobs,make_circles
 
 def get_data_blobs(n_points=100):
-  pass
-  # write your code here
-  # Refer to sklearn data sets
-  X, y = None
-  # write your code ...
+  X, y = make_blobs(n_samples=n_points, centers=3, n_features=2,random_state=0)
   return X,y
 
 def get_data_circles(n_points=100):
-  pass
-  # write your code here
-  # Refer to sklearn data sets
-  X, y = None
-  # write your code ...
+  X, y = make_circles(n_samples = n_points)
   return X,y
 
 def get_data_mnist():
